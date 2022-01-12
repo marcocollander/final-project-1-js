561eb78697d64b362a7874f721b320a122e3cce4
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-01-12 06:53:55 +0100

    Add logical project

---
+++ js/Income.js
@@ -0,0 +1,10 @@
+class Income {
+  constructor(name, aumont) {
+    this.name = name;
+    this.amounr = amount;
+  }
+
+  addIncome() {
+    
+  }
+}
---
+++ js/Model.js
@@ -0,0 +1,12 @@
+class Model {
+  construktor(inputName, inputPrice, inputSum, btnAdd) {
+    this.inputName = inputName;
+    this.inputPrice = inputPrice;
+    this.inputSum = inputSum;
+    this.btnAdd = btnAdd;
+  }
+
+  setInput(inputName, querySelector) {
+    this.inputName = documet.querySelector(selctor)
+  }
+}
