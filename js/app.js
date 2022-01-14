1662891fac759951ded6aa22d150cf47451ec006
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-01-14 23:50:53 +0100

    Add function income in js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 const income = () => {
-  const btnAddIncome = document.querySelector('#incomeBtn');
-  const incomeName = document.querySelector('#incomeName');
-  const incomeAmount = document.querySelector('#incomeAmount');
+  const qs = (selector) => document.querySelector(selector);
+  const btnAddIncome = qs('#incomeBtn');
+
+  const cel = (el) => document.createElement(el);
+
+
+  const addIncomeItem = () => {
+    let incomeName = qs('#incomeName').value;
+    let incomeAmount = qs('#incomeAmount').value;
+    const list = qs('.income__list');
+    list.appendChild(cel('li')).innerText = `${incomeName} ${incomeAmount}`
+  }
 
   btnAddIncome.addEventListener('click', () => {
-    console.log(incomeName.value);
-    console.log(incomeAmount.value);
+    addIncomeItem();
   })
+
+
 
 }
 
