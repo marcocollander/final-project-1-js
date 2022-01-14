45bf445df8551f18b2251dd33d729ef9f7ddec8d
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-01-14 23:08:17 +0100

    Add style for first breakpoint

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+const income = () => {
+  const btnAddIncome = document.querySelector('#incomeBtn');
+  const incomeName = document.querySelector('#incomeName');
+  const incomeAmount = document.querySelector('#incomeAmount');
+
+  btnAddIncome.addEventListener('click', () => {
+    console.log(incomeName.value);
+    console.log(incomeAmount.value);
+  })
+
+}
+
+income();
 
