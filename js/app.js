d00200d2d8162193711240b44351e5205671b3ac
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-01-15 01:30:44 +0100

    Update function income in js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,36 @@
 const income = () => {
-  const qs = (selector) => document.querySelector(selector);
+
+  const qs = selector => document.querySelector(selector);
+  const cel = el => document.createElement(el);
+
   const btnAddIncome = qs('#incomeBtn');
-
-  const cel = (el) => document.createElement(el);
-
 
   const addIncomeItem = () => {
     let incomeName = qs('#incomeName').value;
     let incomeAmount = qs('#incomeAmount').value;
     const list = qs('.income__list');
-    list.appendChild(cel('li')).innerText = `${incomeName} ${incomeAmount}`
-  }
+    list.appendChild(cel('li')).innerText = `${incomeName} ${incomeAmount}`;
+  };
+
+  const addBtns = () => {
+    const aside = qs('.income__btns');
+    const div = cel('div');
+
+    const btnEdit = cel('button');
+    btnEdit.innerText = 'Edytuj';
+    div.appendChild(btnEdit);
+    aside.appendChild(div);
+
+    const btnDelete = cel('button');
+    btnDelete.innerText = 'Usuń';
+    div.appendChild(btnDelete);
+    aside.appendChild(div);
+  };
 
   btnAddIncome.addEventListener('click', () => {
     addIncomeItem();
-  })
-
-
-
-}
+    addBtns();
+  });
+};
 
 income();
-
